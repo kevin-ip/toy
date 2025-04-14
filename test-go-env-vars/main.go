0d4182afb1c95ebbbf7a7195f03d7492b2ef353a
kevin <kevin@seed.im>
2025-04-14 17:41:37 -0500

    Remove printf as the libraries already display GOMAXPROCS and GOMEMLIMIT settings

--- test-go-env-vars/main.go
+++ test-go-env-vars/main.go
@@ -4,21 +4,12 @@ import (
 	"fmt"
 	_ "github.com/KimMachineGun/automemlimit"
 	_ "go.uber.org/automaxprocs"
-	"runtime"
-	"runtime/debug"
 	"time"
 )
 
 func main() {
-
-	fmt.Printf("GOMAXPROCS is set to: %d\n", runtime.GOMAXPROCS(0))
-
-	limit := debug.SetMemoryLimit(-1) // Retrieve the current limit
-	fmt.Printf("GOMEMLIMIT is set to: %d bytes\n", limit)
-
 	done := make(chan struct{})
 
-	// Optional: Do some work in a goroutine
 	go func() {
 		for {
 			fmt.Println("Working in the background...")
@@ -28,5 +19,5 @@ func main() {
 
 	// Block forever
 	<-done
-	fmt.Println("This line is never reached")
+	fmt.Println("This line should never be reached")
 }
